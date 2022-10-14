3fde834872382690ad9a5e2cd3b0ad2872a113be
Ahrve <ahrvekim@bpoc.co.jp>
2022-10-14 14:52:44 +0800

    settings of porifle password and email

--- src/screens/settings/PasswordChange.js
+++ src/screens/settings/PasswordChange.js
@@ -4,7 +4,29 @@ import React from 'react'
 const PasswordChange = () => {
   return (
     <View>
-      <Text>PasswordChange</Text>
+      <View className='relative'>
+        <TextInput className='bg-white py-4 text-right pr-4 border-b border-gray-300 mb-0.5' placeholder='6文字以上半角英数字'
+          />
+        <Text className='absolute pl-4 top-5'>パスワード</Text>
+      </View>
+      <View className='relative'>
+        <TextInput className='bg-white py-4 text-right pr-4 border-b border-gray-300 mb-0.5' placeholder='6文字以上半角英数字'
+          />
+        <Text className='absolute pl-4 top-5'>新しいパスワード</Text>
+      </View>
+      <View className='relative'>
+        <TextInput className='bg-white py-4 text-right pr-4 border-b border-gray-300 mb-0.5' placeholder='もう一度パスワードを入力し...'
+          />
+        <Text className='absolute pl-4 top-5'>確認用パスワード</Text>
+      </View>
+      <View className='flex-row self-center items-center mt-4'>
+        <TouchableOpacity className='py-2 w-11/12 bg-primary rounded-full'>
+            <Text className='text-white text-center'>変更する</Text>
+        </TouchableOpacity>
+      </View>
+      <View className='mt-5'>
+        <Text className='text-center'>パスワードをお忘れの方はこちら</Text>
+      </View>
     </View>
   )
 }
--- src/screens/settings/Profile.js
+++ src/screens/settings/Profile.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <View className='relative'>
+        <TextInput className='bg-white py-4 text-right pr-4 border-b border-gray-300 mb-0.5' placeholder='寺野'
+          />
+        <Text className='absolute pl-4 top-5'>姓</Text>
+      </View>
+      <View className='relative'>
+        <TextInput className='bg-white py-4 text-right pr-4 border-b border-gray-300 mb-0.5' placeholder='慎太郎'
+          />
+        <Text className='absolute pl-4 top-5'>名</Text>
+      </View>
+      <View className='bg-white py-4 border-b border-gray-300 mb-0.5'>
+        <Text className='pl-4'>写真を変更する</Text>
+      </View>
+      <View className='flex-row self-center items-center mt-4'>
+        <TouchableOpacity className='py-2 w-11/12 bg-primary rounded-full'>
+            <Text className='text-white text-center'>変更</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
