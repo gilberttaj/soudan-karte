bcf1462fc0de619082405d62555bea3bdda32a02
Ahrve <ahrve@exodiagamedev.com>
2022-10-17 15:43:51 +0800

    plans

--- src/screens/settings/PaidPlanSignup.js
+++ src/screens/settings/PaidPlanSignup.js
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const PaidPlanSignup = () => {
   return (
-    <View>
-      <Text>PaidPlanSignup</Text>
-    </View>
+    <>
+      <View className="px-4 py-6 bg-white">
+        <View>
+          <Text className="text-base">相談カルテを利用して専門家とのチャットを行う場 合は相談カルテのデータ保管料として月額1,000円 が必要となります。</Text>
+        </View>
+        <View className="flex-col justify-center items-center my-5">
+          <Text className="text-primary text-lg font-medium">データ保管料(定期購読)</Text>
+          <Text className="text-lg font-bold">月額1,000円(税別)</Text>
+        </View>
+        <View className='flex-row self-center items-center mt-4'>
+          <TouchableOpacity className='py-3 w-full bg-primary rounded-xl'>
+              <Text className='text-white text-center'>定期購読を購入する</Text>
+          </TouchableOpacity>
+        </View>
+        <View className='flex-row self-center items-center mt-4'>
+          <TouchableOpacity className='py-3 w-full bg-primary rounded-xl'>
+              <Text className='text-white text-center'>復元する</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="mt-6">
+          <Text className="text-center text-sm">※定期購読解除は<Text className="text-gray-400">⌈</Text>設定<Text className="text-gray-400">|</Text>アプリより行えます</Text>
+        </View>
+      </View>
+    </>
+   
   )
 }
 
--- src/screens/settings/PaidPlans.js
+++ src/screens/settings/PaidPlans.js
@@ -1,11 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const PaidPlans = () => {
+  const navigation = useNavigation();
+  const paidPlanSignup = () => {
+    navigation.navigate('PaidPlanSignup');
+  }
+  const paidPlanInfo = () => {
+    navigation.navigate('PaidPlanInfo')
+  }
   return (
-    <View>
-      <Text>PaidPlans</Text>
-    </View>
+    <>
+      <View className=''>
+        <Text className='py-3 pl-3'>ステータス(定期購読の購読状態)</Text>
+      </View>
+  
+      <View className='bg-white flex-row border-b mb-0.5 border-gray-300'>
+        <Text className='py-3 pl-3 flex-1'>未設定</Text>
+      </View>
+      <View className=''>
+        <Text className='py-3 pl-3'>有料プランの案内</Text>
+      </View>
+      <TouchableOpacity className='bg-white flex-row border-b border-gray-300 mb-0.5' onPress={paidPlanSignup}>
+        <Text className='py-3 pl-3 flex-1'>有料プランに登録する</Text>
+        <View className='self-center pr-2'>
+          <Ionicons  name='chevron-forward-outline' size={15} color='silver'/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity className='bg-white flex-row border-b border-gray-300 mb-0.5' onPress={paidPlanInfo}>
+        <Text className='py-3 pl-3 flex-1'>有料プランとは?</Text>
+        <View className='self-center pr-2'>
+          <Ionicons  name='chevron-forward-outline' size={15} color='silver'/>
+        </View>
+      </TouchableOpacity>
+    </>
   )
 }
 
