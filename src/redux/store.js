261b73247a3fe58f98d6b846b1c925dff200bb74
Ahrve <ahrvekim@bpoc.co.jp>
2022-10-25 11:29:56 +0800

    category3 and confirmation3

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import userReducer from './userSlice'
 import consultationReducer from './consultationSlice'
 import category1Reducer from './category1Slice'
 import category2Reducer from './category2Slice'
+import category3Reducer from './category3Slice'
 
 
 
@@ -12,6 +13,7 @@ export const store = configureStore({
         consultation: consultationReducer,
         category1: category1Reducer,
         category2: category2Reducer,
+        category3: category3Reducer,
     },
 })
 
